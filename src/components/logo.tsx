450ee90fb654b543e890b9d3b6462d522616a32d
aryan <aryanonkar2005@gmail.com>
2025-02-27 22:42:17 +0530

    Initial commit

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -17,7 +17,7 @@ export function Logo({ className, size = 24, withText = true }: LogoProps) {
         <img src="/interchat-blue-no-bg.svg" className="sm:mt-0.5 dark:hidden" width={size} height={size}/>
       </div>
       {withText && (
-        <span className="font-medium text-xl text-primary dark:text-white">InterChat</span>
+        <span className="font-medium text-xl mt-[1px] text-primary dark:text-white">InterChat</span>
       )}
     </div>
   );
