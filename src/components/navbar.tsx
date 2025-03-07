a106ab6c31b1e98e89720ad301b76c6534ae1c1d
aryan <aryanonkar2005@gmail.com>
2025-03-08 00:25:48 +0530

    Initial commit

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,10 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {Logo} from "./logo";
 import {Button} from "@/components/ui/button";
+import {Label} from "@/components/ui/label";
+import {RadioGroup, RadioGroupItem} from "@/components/ui/radio-group";
 import {Avatar, AvatarFallback, AvatarImage} from "@radix-ui/react-avatar";
-import {ChevronDown, ChevronDownIcon, LoaderCircle, Monitor, Moon, Sun, User} from "lucide-react";
+import {ChevronDown, LoaderCircle, Monitor, Moon, Sun, User} from "lucide-react";
 import axios from "axios";
 import {
     DropdownMenu,
@@ -12,7 +14,14 @@ import {
 } from "@/components/ui/dropdown-menu.tsx";
 import {Separator} from "@radix-ui/react-dropdown-menu";
 import {useTheme} from "next-themes";
-import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@radix-ui/react-select";
+import {Select, SelectContent, SelectItem, SelectTrigger} from "@radix-ui/react-select";
+import {
+    Dialog,
+    DialogContent,
+    DialogOverlay,
+    DialogPortal,
+    DialogTitle
+} from "@radix-ui/react-dialog";
 
 enum authenticationStatus {
     LOADING, AUTHENTICATED, UNAUTHENTICATED
@@ -22,6 +31,7 @@ export function Navbar() {
     const {theme, setTheme} = useTheme()
     const [authStatus, setAuthStatus] = useState(authenticationStatus.LOADING);
     const [user, setUser] = useState(null);
+    const [themeDialogVisible, setThemeDialogVisible] = useState(false);
     const getLoggedInUser = async () => {
         try {
             await axios.get('http://localhost:8080/api/user', {withCredentials: true}).then((response) => {
@@ -36,7 +46,9 @@ export function Navbar() {
             setAuthStatus(authenticationStatus.UNAUTHENTICATED);
         }
     }
-    getLoggedInUser();
+    useEffect(() => {
+        getLoggedInUser();
+    },[])
     const logoutBtnClicked = async () => {
         await axios.get('http://localhost:8080/logout', {withCredentials: true}).then((response) => {
             setAuthStatus(authenticationStatus.UNAUTHENTICATED);
@@ -50,6 +62,33 @@ export function Navbar() {
     return (
         <header
             className="sticky top-0 pt-[1px] sm:pt-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+            <Dialog open={themeDialogVisible}>
+                <DialogPortal>
+                    <DialogOverlay className="fixed -inset-1 bg-slate-950/50 data-[state=open]:animate-overlayShow" onClick={()=>setThemeDialogVisible(false)} />
+                    <DialogContent className="fixed left-1/2 top-1/2 max-h-[85vh] min-w-[80vw] -translate-x-1/2 -translate-y-1/2 rounded-3xl bg-secondary px-10 pt-8 pb-10 shadow-xl shadow-black/20 dark:shadow-black/50 focus:outline-none data-[state=open]:animate-contentShow">
+                        <DialogTitle className="m-0 text-xl font-medium text-foreground">
+                            Choose a theme
+                        </DialogTitle>
+                        <RadioGroup className="flex flex-col gap-6 mt-7" onValueChange={setTheme}>
+                            {['light', 'dark', 'system'].map((theme) => (
+                                <div key={theme} className="flex items-center gap-4">
+                                    <RadioGroupItem
+                                        value={theme}
+                                        id={theme}
+                                        className="w-6 h-6 rounded-full border border-gray-300">
+                                    </RadioGroupItem>
+                                    <Label htmlFor={theme} className="capitalize text-xl flex items-center gap-4">
+                                        {theme === 'light'}
+                                        {theme === 'dark'}
+                                        {theme === 'system'}
+                                        {theme}
+                                    </Label>
+                                </div>
+                            ))}
+                        </RadioGroup>
+                    </DialogContent>
+                </DialogPortal>
+            </Dialog>
             <div className="container flex h-16 items-center justify-between py-4">
                 <Logo className="-ml-4 sm:ml-0"/>
                 <div className="-mr-4 sm:mr-0 flex items-center gap-4">
@@ -114,21 +153,32 @@ export function Navbar() {
                                 <span className="text-sm font-semibold text-muted-foreground">{user?.email}</span>
                             </div>
                             <Separator className="bg-input h-[1px] mx-2"/>
+                            <DropdownMenuItem className="mx-1 my-2 px-2.5 py-2 sm:hidden capitalize"
+                                              onClick={() => setThemeDialogVisible(true)}>
+                                Theme: {theme}
+                            </DropdownMenuItem>
                             <DropdownMenuItem className="mx-1 my-2 p-0 hidden sm:flex">
                                 <Select onValueChange={setTheme}>
                                     <SelectTrigger className="w-full text-left flex outline-none px-2.5 py-2">
                                         <a className="capitalize text-foreground px-1">Theme: {theme}</a>
                                         <ChevronDown className="mr-0 ml-auto text-muted-foreground" size={20}/>
                                     </SelectTrigger>
-                                    <SelectContent className="ml-32 mt-40 z-30 shadow-lg dark:shadow-black bg-background border rounded-lg overflow-hidden">
-                                        <SelectItem className="flex items-center text-left gap-4 outline-none py-2.5 px-4 dark:hover:bg-primary/50 hover:bg-primary/25" value="light">
+                                    <SelectContent
+                                        className="ml-32 mt-40 z-30 shadow-lg dark:shadow-black bg-background border rounded-lg overflow-hidden">
+                                        <SelectItem
+                                            className="flex items-center text-left gap-4 outline-none py-2.5 px-4 dark:hover:bg-primary/50 hover:bg-primary/25"
+                                            value="light">
                                             <Sun size={16} strokeWidth={1.5}/>
                                             <span>Light</span>
                                         </SelectItem>
-                                        <SelectItem className="flex items-center text-left gap-4 outline-none py-2.5 px-4 dark:hover:bg-primary/50 hover:bg-primary/25" value="dark">
+                                        <SelectItem
+                                            className="flex items-center text-left gap-4 outline-none py-2.5 px-4 dark:hover:bg-primary/50 hover:bg-primary/25"
+                                            value="dark">
                                             <Moon size={16} strokeWidth={1.5}/>
                                             <span>Dark</span></SelectItem>
-                                        <SelectItem className="flex items-center text-left gap-4 outline-none py-2.5 px-4 dark:hover:bg-primary/50 hover:bg-primary/25" value="system">
+                                        <SelectItem
+                                            className="flex items-center text-left gap-4 outline-none py-2.5 px-4 dark:hover:bg-primary/50 hover:bg-primary/25"
+                                            value="system">
                                             <Monitor size={16} strokeWidth={1.5}/>
                                             <span>System</span>
                                         </SelectItem>
